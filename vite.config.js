02222893d2d6cb0fe2579c3d5f1ce3e829db44df
Anushka Jadhav <150259274+anushkasj06@users.noreply.github.com>
2025-01-11 16:40:06 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0',  // Allow external traffic
+    port: process.env.PORT || 3000,  // Use the environment port or fallback to 3000
+  },
   plugins: [react()],
 })
